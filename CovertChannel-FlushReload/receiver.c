53aa0227f0ad6008fe430169dab497383602a0f6
sforker1 <sforker1@binghamton.edu>
2022-04-07 13:12:08 -0400

    Added additional file send argument (Needs Work)

--- CovertChannel-FlushReload/receiver.c
+++ CovertChannel-FlushReload/receiver.c
@@ -26,11 +26,81 @@ bool detect_bit(struct config *config)
 	return misses >= hits;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) { // Append to end of arguments " -s filename"
+	//TODO Error for argc < 2
+	setbuf(stdout, NULL);
+	char* fileSendMSG = NULL;
+	int ifFileSend = 0; //Checks for additional file argument
+	int k = 0;
+	while (k < argc) {
+		if(strcmp(argv[k],"-s") == 0) {
+			ifFileSend = 1;
+		}
+		k++;
+	}
+
 	// Initialize config and local variables
 	struct config config;
-	init_config(&config, argc, argv);
+
+	char file_buf[128];
+
+	if(ifFileSend) { //Grabs additional file argument
+		char** new_argv = malloc((argc-1) * sizeof *new_argv);
+		int new_argc = argc - 2;
+		for(int i = 0; i < new_argc; i++) {
+			size_t length = strlen(argv[i])+1;
+        	new_argv[i] = malloc(length);
+        	memcpy(new_argv[i], argv[i], length);
+		}
+		new_argv[new_argc] = NULL;
+		
+		strcpy(file_buf, argv[argc - 1]);
+
+		int k = 0;
+		while(k < new_argc) {
+			printf("%s ", new_argv[k]);
+			k++;
+		}
+		//printf("\n%s", file_buf);
+
+		init_config(&config, new_argc, new_argv);
+	} else {
+		init_config(&config, argc, argv);
+	}
+
+	char *str = NULL; // Entire file contents
+
+	if(ifFileSend) { // Grabs file for comparison (converts to binary)
+		char buf[4096];
+    	ssize_t n;
+	    size_t len = 0;
+		int file;
+		file = open(file_buf, O_RDONLY);
+    	while (n = read(file, buf, sizeof buf)) {
+        	if (n < 0) {
+            	if (errno == EAGAIN)
+                	continue;
+            	perror("read");
+            	break;
+        	}
+        	str = realloc(str, len + n + 1);
+        	memcpy(str + len, buf, n);
+        	len += n;
+        	str[len] = '\0';
+    	}
+
+		// Binary Conversion
+		
+		// size_t msg_len = strlen(msg);
+		// for (int ind = 0; ind < msg_len; ind++) {
+		// 	if (msg[ind] == '0') {
+		// 		send_bit(false, &config);
+		// 	} else {
+		// 		send_bit(true, &config);
+		// 	}
+		// }
+	}
+
 	char msg_ch[MAX_BUFFER_LEN + 1];
 
 	uint32_t bitSequence = 0;
@@ -66,12 +136,29 @@ int main(int argc, char **argv)
 			int ascii_msg_len = binary_msg_len / 8;
 			char msg[ascii_msg_len];
 			printf("> %s\n", conv_char(msg_ch, ascii_msg_len, msg));
+
+			if(ifFileSend) {
+				size_t length = strlen(msg_ch)+1;
+        		fileSendMSG = malloc(length);
+        		memcpy(fileSendMSG, msg_ch, length);
+			}
 	
 			// Terminate loop if received "exit" message
 			if (strcmp(msg, "exit") == 0) {
 				break;
 			}
 		}
+	}
+
+	if(ifFileSend) {
+		int correctBits = 0;
+		int totalBits = 0;
+		//for(int i = 0; i < strlen(source); i++) {
+
+		//}
+		printf("\nERROR RATE: %d correct bits out of %d total bits\n", correctBits, totalBits);
+		//printf("\n\nTHE SENT FILE WAS: %s", fileSendMSG);
+
 	}
 
 	printf("Receiver finished\n");
