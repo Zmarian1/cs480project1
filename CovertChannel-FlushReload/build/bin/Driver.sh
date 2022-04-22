38fd88a4f6712ec6fa2a5c121d652d99c8668f5e
sforker1 <sforker1@binghamton.edu>
2022-04-22 16:24:16 -0400

    done

--- CovertChannel-FlushReload/build/bin/Driver.sh
+++ CovertChannel-FlushReload/build/bin/Driver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Runs the sender and receiver with input file input.txt
-taskset -c 1 ./receiver -s input.txt &
-taskset -c 1 ./sender -s input.txt
+./receiver -s input.txt &
+./sender -s input.txt
